fd34fabaef1c8968db221560acc3af39e37e6a83
nicolasvondolinger <nicolasvondolinger@gmail.com>
2023-06-10 13:04:04 -0300

    versao final do vpl13

--- vpl13/jogo_da_vida.cpp
+++ vpl13/jogo_da_vida.cpp
@@ -1,1 +1,70 @@
 #include "jogo_da_vida.h"
+
+using namespace std;
+
+JogoDaVida::JogoDaVida(int l, int c){
+    vivas_.resize(l, std::vector<bool>(c));
+}
+
+bool JogoDaVida::viva(int i, int j){
+    return vivas_[i][j];
+}
+
+void JogoDaVida::Matar(int i, int j){
+    vivas_[i][j] = false;
+}
+
+void JogoDaVida::Reviver(int i, int j){
+    vivas_[i][j] = true;
+}
+
+int JogoDaVida::NumeroDeVizinhasVivas(int x, int y) {
+    int count = 0;
+
+    // Definir as posições relativas das células vizinhas
+    int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+    // Percorrer as células vizinhas
+    for (int i = 0; i < 8; i++) {
+        int nx = (x + dx[i] + linhas()) % linhas();    // Lidar com o torus nas bordas
+        int ny = (y + dy[i] + colunas()) % colunas();  // Lidar com o torus nas bordas
+
+        if (viva(nx, ny)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+void JogoDaVida::ExecutarProximaIteracao() {
+    // Criar uma cópia do estado atual das células
+    vector<vector<bool>> novoEstado = vivas_;
+
+    // Percorrer todas as células do torus
+    for (int i = 0; i < linhas(); i++) {
+        for (int j = 0; j < colunas(); j++) {
+            // Obter o número de vizinhas vivas da célula [i, j]
+            int numVizinhasVivas = NumeroDeVizinhasVivas(i, j);
+
+            // Verificar as regras do Jogo da Vida
+            if (viva(i, j)) {
+                // Se a célula está viva
+                if (numVizinhasVivas < 2 || numVizinhasVivas > 3) {
+                    // A célula morre de solidão ou superpopulação
+                    novoEstado[i][j] = false;
+                }
+            } else {
+                // Se a célula está morta
+                if (numVizinhasVivas == 3) {
+                    // A célula é revivida
+                    novoEstado[i][j] = true;
+                }
+            }
+        }
+    }
+
+    // Atualizar o estado das células com o novo estado
+    vivas_ = novoEstado;
+}
