c9b53736ba426a3e03f70f6c78b74e3903032d83
nicolasvondolinger <nicolasvondolinger@gmail.com>
2023-06-12 15:46:26 -0300

    adicionando pastas

---
+++ vpl14b/dicionario.cpp
@@ -0,0 +1,61 @@
+#include "dicionario.h"
+#include <list>
+#include <string>
+
+using std::list;
+using std::string;
+
+Dicionario::Dicionario(){
+
+}
+
+int Dicionario::tamanho(){
+    int a = 0;
+    for(auto it = elementos_.begin(); it != elementos_.end(); it++) a++;
+    return a;
+}
+
+bool Dicionario::pertence(string chave){
+    for(auto it : elementos_){
+        if (it.chave == chave) return true;
+    }
+    return false;
+}
+
+string Dicionario::menor(){
+    string menorChave = elementos_.front().chave;
+    for (auto element : elementos_) {
+        if (element.chave < menorChave) menorChave = element.chave;
+    }
+
+    return menorChave;
+}
+
+string Dicionario::valor(string chave){
+    for(auto it : elementos_){
+        if (it.chave == chave) return it.valor;
+    }
+}
+
+void Dicionario::Inserir(string chave, string valor){
+    Elemento toInsert;
+    toInsert.chave = chave;
+    toInsert.valor = valor;
+    elementos_.push_back(toInsert);
+}
+
+void Dicionario::Remover(string chave){
+    for(auto element : elementos_){
+        if(element.chave == chave) elementos_.remove(element);
+    }
+}
+
+void Dicionario::Alterar(string chave, string valor){
+    for(auto element : elementos_){
+        if(element.chave == chave) element.valor = valor;
+    }
+}
+
+Dicionario::~Dicionario(){
+    elementos_.clear();
+}
