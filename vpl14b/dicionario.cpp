31a9549f75ba4396c3fa3584e11c4b48a43a98e7
nicolasvondolinger <nicolasvondolinger@gmail.com>
2023-06-12 19:28:53 -0300

    atualizando vpl14b

--- vpl14b/dicionario.cpp
+++ vpl14b/dicionario.cpp
@@ -5,9 +5,7 @@
 using std::list;
 using std::string;
 
-Dicionario::Dicionario(){
-
-}
+Dicionario::Dicionario(){}
 
 int Dicionario::tamanho(){
     int a = 0;
@@ -16,43 +14,45 @@ int Dicionario::tamanho(){
 }
 
 bool Dicionario::pertence(string chave){
-    for(auto it : elementos_){
-        if (it.chave == chave) return true;
+    for(auto it = elementos_.begin(); it != elementos_.end(); it++){
+        if(it -> chave == chave) return true;
     }
+
     return false;
 }
 
 string Dicionario::menor(){
     string menorChave = elementos_.front().chave;
-    for (auto element : elementos_) {
-        if (element.chave < menorChave) menorChave = element.chave;
+    for(auto it = elementos_.begin(); it != elementos_.end(); it++){
+        if(it -> chave < menorChave) menorChave = it -> chave;
     }
 
     return menorChave;
 }
 
 string Dicionario::valor(string chave){
-    for(auto it : elementos_){
-        if (it.chave == chave) return it.valor;
+    for(auto it = elementos_.begin(); it != elementos_.end(); it++){
+        if(it -> chave == chave) return it -> valor;
     }
 }
 
 void Dicionario::Inserir(string chave, string valor){
-    Elemento toInsert;
-    toInsert.chave = chave;
-    toInsert.valor = valor;
+    Elemento toInsert{chave, valor};
     elementos_.push_back(toInsert);
 }
 
 void Dicionario::Remover(string chave){
-    for(auto element : elementos_){
-        if(element.chave == chave) elementos_.remove(element);
+    for(auto it = elementos_.begin(); it != elementos_.end(); it++){
+        if(it -> chave == chave) {
+            elementos_.erase(it);
+            break;
+        }
     }
 }
 
 void Dicionario::Alterar(string chave, string valor){
-    for(auto element : elementos_){
-        if(element.chave == chave) element.valor = valor;
+    for(auto it = elementos_.begin(); it != elementos_.end(); it++){
+        if(it -> chave == chave) it -> valor = valor;
     }
 }
 
