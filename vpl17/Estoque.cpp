0fe6bee4fb08b6698f3b6634b218e4ddbb04ddc3
nicolasvondolinger <nicolasvondolinger@gmail.com>
2023-06-25 19:06:49 -0300

    outros metodos

--- vpl17/Estoque.cpp
+++ vpl17/Estoque.cpp
@@ -31,24 +31,45 @@ unsigned int Estoque::get_qtd(const std::string& mercadoria) const {
 
 void Estoque::imprime_estoque() const {
   // TODO
+  for(auto it = estoque_.begin(); it != estoque_.end(); it++){
+    cout << it -> first << ", " << it -> second << endl;
+  }
 }
 
 Estoque& Estoque::operator += (const Estoque& rhs) {
   // TODO
+  for (auto it = rhs.estoque_.begin(); it != rhs.estoque_.end(); ++it) {
+    const std::string& mercadoria = it->first;
+    int quantidade = it->second;
+    add_mercadoria(mercadoria, quantidade);
+  }
   return *this;
 }
 
 Estoque& Estoque::operator -= (const Estoque& rhs) {
   // TODO
+  for (auto it = rhs.estoque_.begin(); it != rhs.estoque_.end(); ++it) {
+    const std::string& mercadoria = it->first;
+    int quantidade = it->second;
+    sub_mercadoria(mercadoria, quantidade);
+  }
   return *this;
 }
 
 bool operator < (Estoque& lhs, Estoque& rhs) {
   // TODO
-  return false;
+  for(auto it = lhs.estoque_.begin(); it != lhs.estoque_.end(); ++it){
+    if(rhs.estoque_.find(it -> first) == rhs.estoque_.end()) return false;
+    if(rhs.estoque_[it -> first] < it -> second) return false;
+  }
+  return true;
 }
 
 bool operator > (Estoque& lhs, Estoque& rhs) {
   // TODO
-  return false;
+  for(auto it = lhs.estoque_.begin(); it != lhs.estoque_.end(); ++it){
+    if(rhs.estoque_.find(it -> first) == rhs.estoque_.end()) return false;
+    if(rhs.estoque_[it -> first] > it -> second) return false;
+  }
+  return true;
 }
